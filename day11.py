0b7fc7462a442993c57caf294b26b206c9ae7bbf
Hugo Correia <hugo@koizo.net>
2024-12-11 14:20:22 +0000

    readable code

--- day11.py
+++ day11.py
@@ -1,18 +1,26 @@
+import time
 import functools
+
+start_time = time.time()
 
 with open("day11.txt") as file:
     stones = file.readline().split()
 
+@functools.cache
 def process_stone(stone):
-    return (
-        ["1"] if int(stone) == 0 else
-        [stone[:len(stone) // 2], str(int(stone[len(stone) // 2:]))] if len(stone) % 2 == 0 else
-        [str(int(stone) * 2024)]
-    )
+    if int(stone) == 0:
+        return ["1"]
+    if len(stone) % 2 == 0:
+        mid = len(stone) // 2
+        return [stone[:mid], str(int(stone[mid:]))]
+    return [str(int(stone) * 2024)]
 
 @functools.cache
 def count_stones(stone, blinks):
-    return 1 if blinks == 0 else sum(count_stones(new_stone, blinks - 1) for new_stone in process_stone(stone))
+    if blinks == 0:
+        return 1
+    new_stones = process_stone(stone)
+    return sum([count_stones(new_stone, blinks - 1) for new_stone in new_stones])
 
 print(f"Part 1: {sum([count_stones(stone, 25) for stone in stones])}")
 print(f"Part 2: {sum([count_stones(stone, 75) for stone in stones])}")
